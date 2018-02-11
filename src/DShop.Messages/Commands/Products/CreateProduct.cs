04ecc6c5ec00bc64aa291b2d96adebedafd18aae
GooRiOn <foreverframenet@gmail.com>
2018-02-11 22:20:02 +0100

    Changed ctors od commands to private

--- src/DShop.Messages/Commands/Products/CreateProduct.cs
+++ src/DShop.Messages/Commands/Products/CreateProduct.cs
@@ -1,10 +1,24 @@
+using Newtonsoft.Json;
+using System;
+
 namespace DShop.Messages.Commands.Products
 {
 	public class CreateProduct : ICommand
 	{
+        public Guid Id { get; }
         public string Name { get; }
         public string Descirption { get; }
         public string Vendor { get; }
         public decimal Price { get; }
+        
+        [JsonConstructor]
+        private CreateProduct(Guid? id, string name, string description, string vendor, decimal price)
+        {
+            Id = id ?? Guid.NewGuid();
+            Name = name;
+            Descirption = description;
+            Vendor = vendor;
+            Price = price;
+        }
 	}
 }
--- src/DShop.Messages/Commands/Products/UpdateProduct.cs
+++ src/DShop.Messages/Commands/Products/UpdateProduct.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace DShop.Messages.Commands.Products
 {
-	public class EditProduct : ICommand
+	public class UpdateProduct : ICommand
 	{
         public Guid Id { get; }
         public string Name { get; }
