76d49778e03c4a9442a8bbdee9b8343f42d72a54
GooRiOn <foreverframenet@gmail.com>
2018-02-11 20:20:41 +0100

    Removed handlers.

--- src/DShop.Messages/Commands/ICommandHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DShop.Messages.Commands
-{
-    public interface ICommandHandler<in T> where T : ICommand
-    {
-        Task HandleAsync(T command);
-    }
-}
--- src/DShop.Messages/Events/IEventHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DShop.Messages.Events
-{
-    public interface IEventHandler<in T> where T : IEvent
-    {
-        Task HandleAsync(T @event);
-    }
-}
