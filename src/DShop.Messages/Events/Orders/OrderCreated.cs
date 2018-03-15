a098f01828b6bf04f8aa486154d2fc0761d477d4
GooRiOn <foreverframenet@gmail.com>
2018-03-15 19:11:01 +0100

    Extended OrderCreated event.

--- src/DShop.Messages/Events/Orders/OrderCreated.cs
+++ src/DShop.Messages/Events/Orders/OrderCreated.cs
@@ -6,11 +6,15 @@ namespace DShop.Messages.Events.Orders
     public class OrderCreated : IEvent
     {
         public Guid Id { get; }
+        public Guid CustomerId { get; }
+        public long Number { get; }
 
         [JsonConstructor]
-        public OrderCreated(Guid id)
+        public OrderCreated(Guid id, Guid customerId, long number)
         {
             Id = id;
+            CustomerId = customerId;
+            Number = number;
         }
     }
 }
