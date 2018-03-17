6615ed4f67341187ee1659bf327f6189fef7c1df
GooRiOn <foreverframenet@gmail.com>
2018-03-17 17:56:12 +0100

    Changed CreateOrder command

--- src/DShop.Messages/Commands/Orders/CreateOrder.cs
+++ src/DShop.Messages/Commands/Orders/CreateOrder.cs
@@ -1,6 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 
 namespace DShop.Messages.Commands.Orders
 {
@@ -8,20 +7,12 @@ namespace DShop.Messages.Commands.Orders
     {
         public Guid Id { get; }
         public Guid CustomerId { get; }
-        public long Number { get; }
-        public IEnumerable<Guid> ProductIds { get; }
-        public decimal TotalAmount { get; }
-        public string Currency { get; }
 
         [JsonConstructor]
-        private CreateOrder(Guid id, Guid customerId, long number, IEnumerable<Guid> productIds, decimal totalAmount, string currency)
+        private CreateOrder(Guid id, Guid customerId)
         {
             Id = id;
             CustomerId = customerId;
-            Number = number;
-            ProductIds = productIds;
-            TotalAmount = totalAmount;
-            Currency = currency;
         }
     }
 }
